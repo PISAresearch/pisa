8cd91ae81748bd2c54d492dc199410cf58a19fb9
Chris Buckland <cpbuckland88@gmail.com>
2019-02-19 09:34:07 -0400

    Added example raiden startup command

--- raiden_demo/startup-1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./raiden --gas-price fast --accept-disclaimer --api-address 127.0.0.1:6660 --network-id ropsten --eth-rpc-endpoint https://ropsten.infura.io/v3/6a750ee18d924477b219e6cea6de2215 --address 0x7170BbcEcE1F350Ff3d18199fa37cD7CD0c60094 --password-file ./password-1.txt
---
+++ raiden_demo/startup-example.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./raiden --gas-price fast --accept-disclaimer --api-address 127.0.0.1:6660 --network-id ropsten --eth-rpc-endpoint https://ropsten.infura.io/v3/<token> --address 0x7170BbcEcE1F350Ff3d18199fa37cD7CD0c60094 --password-file ./password-1.txt
