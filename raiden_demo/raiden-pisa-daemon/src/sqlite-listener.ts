cd032ba2017e0024f64324cad4ec72a210aec8a9
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-03-21 16:15:37 +0000

    Make a copy of the db every time it is polled

--- raiden_demo/raiden-pisa-daemon/src/sqlite-listener.ts
+++ raiden_demo/raiden-pisa-daemon/src/sqlite-listener.ts
@@ -1,4 +1,5 @@
 // polls the raiden sqlite db for new events looking for balance proofs
+import { copyFile } from 'fs';
 import * as sqlite from "sqlite3";
 import { IRawBalanceProof } from "./balanceProof";
 
@@ -9,23 +10,38 @@ export interface IBalanceProofRecord {
 
 export class SqliteListener {
     private lastRowId: number;
-    private readonly sqliteDb: sqlite.Database;
 
     constructor(
         private pollInterval: number,
-        dbFileLocation: string,
+        private dbFileLocation: string,
         startingRowId: number,
         private balanceProofCallback: (bp: IRawBalanceProof) => Promise<void>
     ) {
-        this.sqliteDb = new sqlite.Database(dbFileLocation, sqlite.OPEN_READONLY);
         this.lastRowId = startingRowId;
+    }
+
+    // Makes a local copy of the database, and opens it.
+    // Ugly hack needed when running the daemon and raiden within docker containers,
+    // where attempts to access the db would return "SQLITE_BUSY" errors.
+    private async getDbCopy(): Promise<sqlite.Database> {
+        await new Promise((resolve, reject) => {
+            console.log(`Making a copy of ${this.dbFileLocation}`);
+            copyFile(this.dbFileLocation, 'dbcopy.db', (err) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve();
+            });
+        });
+        return new sqlite.Database('dbcopy.db', sqlite.OPEN_READONLY);
     }
 
     public async start() {
         if (this.lastRowId === null) {
+            const sqliteDb = await this.getDbCopy();
             // Find the current lastRowId, then start polling
             this.lastRowId = await new Promise((resolve, reject) => {
-                this.sqliteDb.get("SELECT MAX(s.identifier) as lastRowId FROM state_changes s", [], (err, result) => {
+                sqliteDb.get("SELECT MAX(s.identifier) as lastRowId FROM state_changes s", [], (err, result) => {
                     if (err) {
                         reject(err);
                     } else {
@@ -33,16 +49,20 @@ export class SqliteListener {
                     }
                 });
             });
+
+            sqliteDb.close();
         }
 
         setInterval(this.tick, this.pollInterval, this);
     }
 
-    private tick(listener: SqliteListener) {
-        listener.sqliteDb.serialize((async () => {
+    private async tick(listener: SqliteListener) {
+        const sqliteDb = await listener.getDbCopy();
+
+        sqliteDb.serialize((async () => {
             try {
                 console.log("polling for updates")
-                let results = await SqliteListener.getBalanceProofsSince(listener.lastRowId, listener.sqliteDb);
+                let results = await SqliteListener.getBalanceProofsSince(listener.lastRowId, sqliteDb);
 
                 // process the results synchronously
                 for (let i = 0; i < results.length; i++) {
@@ -55,6 +75,8 @@ export class SqliteListener {
             } catch (err) {
                 console.error(err);
             }
+
+            sqliteDb.close();
         }));
     }
 
