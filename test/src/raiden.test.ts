89befc2a1b000db88cdc9d15c0c034df6c416b6b
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-03-27 23:22:29 +0000

    Several improvements to raiden's end-2-end tests

--- test/src/raiden.test.ts
+++ test/src/raiden.test.ts
@@ -7,9 +7,10 @@ import kill from 'tree-kill';
 
 import request from "request-promise";
 
-import { ethers } from "ethers";
+import { ethers, Contract } from "ethers";
 
-import { exec } from 'child_process';
+import { exec, ChildProcess } from 'child_process';
+import { BigNumber } from "ethers/utils";
 
 function timeout(ms: number): Promise<any> {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -34,12 +35,13 @@ const dbFileName = ".raiden/node_ccca21b9/netid_3/network_ca70bfde/v16_log.db";
 
 
 
-let provider;
+let provider: ethers.providers.Provider;
 
-let testTokenAddr;
-let tokenContract;
-let alice;
-let aliceTokenBalance_start, bobTokenBalance_start;
+let testTokenAddr: string;
+let tokenContract: Contract;
+let subprocesses = [];
+let parity: ChildProcess, alice: ChildProcess, bob: ChildProcess, pisa: ChildProcess, daemon: ChildProcess;
+let aliceTokenBalance_start: BigNumber, bobTokenBalance_start: BigNumber;
 
 const ERC20abi = [
     {
@@ -74,12 +76,12 @@ describe("Raiden end-to-end tests for scenario 2 (with Pisa)", function() {
         await fse.mkdirSync(`${demoDir}/.raiden`);
 
         //Start parity node
-        const parity = exec(`docker-compose -f ${demoDir}/docker/parity-loaded.docker-compose.yml up`);
+        parity = exec(`docker-compose -f ${demoDir}/docker/parity-loaded.docker-compose.yml up`);
         const parityLogStream = await fse.createWriteStream('./parity.test.log', {flags: 'a'});
         parity.stdout.pipe(parityLogStream);
         parity.stderr.pipe(parityLogStream);
 
-        await timeout(3000);
+        await timeout(5000);
 
         // args are redundant, but if they are removed the provider unpredictably fails
         // throwing "Error: invalid response - 0"; see https://github.com/ethers-io/ethers.js/issues/362
@@ -89,6 +91,7 @@ describe("Raiden end-to-end tests for scenario 2 (with Pisa)", function() {
         console.log("Starting Alice");
         const aliceCmd = `${demoDir}/raiden --gas-price fast --accept-disclaimer --keystore-path ${demoDir}/docker/test-accounts --datadir ${demoDir}/.raiden --network-id ropsten --eth-rpc-endpoint http://localhost:8545 --address 0x${aliceAddr} --api-address http://0.0.0.0:6662 --password-file ${demoDir}/docker/test-accounts/password--${aliceAddrLow}.txt  --no-sync-check --disable-debug-logfile --tokennetwork-registry-contract-address 0xCa70BfDEa6BD82e45d4fD26Dd9f36DB9fad61796 --secret-registry-contract-address 0xaFa1F14fe33940b22D7f9F9bf0d707860C9233e2 --endpoint-registry-contract-address 0xa4f842B60C8a21c54b16E7940aA16Dda80301d13`;
         alice = exec(aliceCmd);
+        subprocesses.push(alice);
         const aliceLogStream = await fse.createWriteStream('./alice.test.log', {flags: 'a'});
         alice.stdout.pipe(aliceLogStream);
         alice.stderr.pipe(aliceLogStream);
@@ -97,16 +100,18 @@ describe("Raiden end-to-end tests for scenario 2 (with Pisa)", function() {
         //Start raiden node for Bob
         console.log("Starting Bob");
         const bobCmd = `${demoDir}/raiden --gas-price fast --accept-disclaimer --keystore-path ${demoDir}/docker/test-accounts --datadir ${demoDir}/.raiden --network-id ropsten --eth-rpc-endpoint http://localhost:8545 --address 0x${bobAddr} --api-address http://0.0.0.0:6663 --password-file ${demoDir}/docker/test-accounts/password--${bobAddrLow}.txt  --no-sync-check --disable-debug-logfile --tokennetwork-registry-contract-address 0xCa70BfDEa6BD82e45d4fD26Dd9f36DB9fad61796 --secret-registry-contract-address 0xaFa1F14fe33940b22D7f9F9bf0d707860C9233e2 --endpoint-registry-contract-address 0xa4f842B60C8a21c54b16E7940aA16Dda80301d13`;
-        const bob = exec(bobCmd);
+        bob = exec(bobCmd);
+        subprocesses.push(bob);
         const bobLogStream = await fse.createWriteStream('./bob.test.log', {flags: 'a'});
         bob.stdout.pipe(bobLogStream);
         bob.stderr.pipe(bobLogStream);
 
         //Start Pisa
         console.log("Starting Pisa");
-        const pisa = exec(`docker run -p 3000:3000 --network docker_raidendemo --network-alias pisa -v ${pisaRoot}/configs/parity.json:/usr/pisa/build/config.json pisaresearch/pisa:latest`, (err) => {
+        pisa = exec(`docker run -p 3000:3000 --network docker_raidendemo --network-alias pisa -v ${pisaRoot}/configs/parity.json:/usr/pisa/build/config.json pisaresearch/pisa:latest`, (err) => {
             throw err;
         });
+        subprocesses.push(pisa);
         const pisaLogStream = await fse.createWriteStream('./pisa.test.log', {flags: 'a'});
         pisa.stdout.pipe(pisaLogStream);
         pisa.stderr.pipe(pisaLogStream);
@@ -116,9 +121,10 @@ describe("Raiden end-to-end tests for scenario 2 (with Pisa)", function() {
 
         console.log("Starting the daemon");
         //Start raiden-pisa-daemon for Alice
-        const daemon = exec(`docker run -v ${demoDir}/docker/test-accounts/password--${aliceAddrLow}.txt:/home/password.txt -v ${demoDir}/docker/test-accounts/UTC--2019-03-22T10-39-56.702Z--0x${aliceAddrLow}:/.ethereum/keystore/UTC--2019-03-22T10-39-56.702Z--0x${aliceAddrLow} -v ${demoDir}/${dbFileName}:/home/db --network docker_raidendemo --entrypoint "npm" pisaresearch/raiden-pisa-daemon:latest run start -- --pisa=pisa:3000 --keyfile=/.ethereum/keystore/UTC--2019-03-22T10-39-56.702Z--0x${aliceAddrLow}  --password-file=/home/password.txt --db=/home/db`, (err) => {
+        daemon = exec(`docker run -v ${demoDir}/docker/test-accounts/password--${aliceAddrLow}.txt:/home/password.txt -v ${demoDir}/docker/test-accounts/UTC--2019-03-22T10-39-56.702Z--0x${aliceAddrLow}:/.ethereum/keystore/UTC--2019-03-22T10-39-56.702Z--0x${aliceAddrLow} -v ${demoDir}/${dbFileName}:/home/db --network docker_raidendemo --entrypoint "npm" pisaresearch/raiden-pisa-daemon:latest run start -- --pisa=pisa:3000 --keyfile=/.ethereum/keystore/UTC--2019-03-22T10-39-56.702Z--0x${aliceAddrLow}  --password-file=/home/password.txt --db=/home/db`, (err) => {
             throw err;
         });
+        subprocesses.push(daemon);
         const daemonLogStream = await fse.createWriteStream('./daemon.test.log', {flags: 'a'});
         daemon.stdout.pipe(daemonLogStream);
         daemon.stderr.pipe(daemonLogStream);
@@ -151,20 +157,27 @@ describe("Raiden end-to-end tests for scenario 2 (with Pisa)", function() {
         aliceTokenBalance_start = await tokenContract.balanceOf(`0x${aliceAddr}`);
         bobTokenBalance_start = await tokenContract.balanceOf(`0x${bobAddr}`);
         console.log("Initial token balances: ", aliceTokenBalance_start, bobTokenBalance_start);
+    });
 
-
+    afterEach(async () => {
+        console.log("CLEANUP");
         //Cleanup on exit
-        process.on('exit', function() {
-            const children = [daemon, pisa, bob, alice, parity]
-            console.log('killing', children.length, 'child processes');
-            children.forEach(function(child) {
-              child.kill();
-            });
+        const promises = [];
+        subprocesses.forEach((child) => {
+            promises.push(new Promise(resolve => {
+                try {
+                    kill(child.pid, resolve);
+                } catch(err) { 
+                    // Ignore error on cleanup
+                    console.log(err);
+                }
+            }))
+        });
+        await Promise.all(promises);
+        subprocesses = [];
 
-            //TODO: delete raiden db?
-          });
-        
-        console.log("Done setting up");
+        //TODO: figure out why this is not completely shutting off the containers
+        exec(`docker-compose -f ${demoDir}/docker/parity-loaded.docker-compose.yml down --rmi all`);
     });
 
     it("completes scenario 2 correctly", async () => {
@@ -198,13 +211,12 @@ describe("Raiden end-to-end tests for scenario 2 (with Pisa)", function() {
         console.log("Payment result:", paymentResult);
 
         // Wait to give time to daemon and Pisa to pick up the appointment 
-        await timeout(20 * blockTime);
+        await timeout(10 * blockTime);
 
         // Shutdown Alice
         await new Promise(resolve => kill(alice.pid, 'SIGKILL', resolve));
 
         // Bob closes the channel
-        // Make a payment from Bob to Alice
         const closeChannelResult = await request({
             method: "PATCH",
             uri: `http://0.0.0.0:6663/api/v1/channels/${testTokenAddr}/0x${aliceAddr}`,
@@ -224,9 +236,9 @@ describe("Raiden end-to-end tests for scenario 2 (with Pisa)", function() {
         const bobTokenBalance_end = await tokenContract.balanceOf(`0x${bobAddr}`);
         console.log("Token balances: ", aliceTokenBalance_end, bobTokenBalance_end);
 
-
-        //TODO: fix the comparison
+        //Test if the final balance of Alice includes the payment from Bob
         assert(aliceTokenBalance_start.add(paymentAmount).eq(aliceTokenBalance_end), "Pisa saved the day.");
-    });
 
+        console.log("DONE WITH TESTCASE");
+    });
 });
