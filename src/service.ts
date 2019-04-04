4a6697abdb9f781b924562b829652d08998a6498
Chris Buckland <cpbuckland88@gmail.com>
2019-04-04 14:43:27 +0200

    Moved tower location

--- src/service.ts
+++ src/service.ts
@@ -1,58 +1,16 @@
 import express, { Response } from "express";
 import httpContext from "express-http-context";
 import logger from "./logger";
-import { Appointment, PublicInspectionError, PublicDataValidationError } from "./dataEntities";
-import { Inspector } from "./inspector";
-import { Raiden, Kitsune, IChannelConfig } from "./integrations";
+import { PublicInspectionError, PublicDataValidationError } from "./dataEntities";
+import { Raiden, Kitsune } from "./integrations";
 import { Watcher } from "./watcher";
+import { PisaTower } from "./tower";
 // PISA: this isn working properly, it seems that watchers are sharing the last set value...
 import { setRequestId } from "./customExpressHttpContext";
 import { Server } from "http";
 import { inspect } from "util";
 import { ethers } from "ethers";
 import { Responder } from "./responder";
-
-/**
- * A PISA tower, configured to watch for specified appointment types
- */
-class PisaTower {
-    constructor(
-        public readonly provider: ethers.providers.Provider,
-        public readonly watcher: Watcher,
-        channelConfigs: IChannelConfig<Appointment, Inspector<Appointment>>[]
-    ) {
-        channelConfigs.forEach(c => (this.configs[c.channelType] = c));
-    }
-
-    configs: {
-        [type: number]: IChannelConfig<Appointment, Inspector<Appointment>>;
-    } = {};
-
-    /**
-     * Checks that the object is well formed, that it meets the conditions necessary for watching and assigns it to be watched.
-     * @param obj
-     */
-    async addAppointment(obj: any) {
-        if (!obj) throw new PublicDataValidationError("No content specified.");
-
-        // look for a type argument
-        const type = obj["type"];
-        const config = this.configs[type];
-        if (!config) throw new PublicDataValidationError(`Unknown appointment type ${type}.`);
-
-        // parse the appointment
-        const appointment = config.appointment(obj);
-
-        const inspector = config.inspector(10, this.provider);
-        // inspect this appointment, an error is thrown if inspection is failed
-        await inspector.inspectAndPass(appointment);
-
-        // start watching it if it passed inspection
-        await this.watcher.addAppointment(appointment);
-
-        return appointment;
-    }
-}
 
 /**
  * Hosts a PISA service at the endpoint.
---
+++ src/tower.ts
@@ -0,0 +1,47 @@
+import { Appointment, PublicDataValidationError } from "./dataEntities";
+import { Inspector } from "./inspector";
+import { IChannelConfig } from "./integrations";
+import { Watcher } from "./watcher";
+import { ethers } from "ethers";
+
+/**
+ * A PISA tower, configured to watch for specified appointment types
+ */
+export class PisaTower {
+    constructor(
+        public readonly provider: ethers.providers.Provider,
+        public readonly watcher: Watcher,
+        channelConfigs: IChannelConfig<Appointment, Inspector<Appointment>>[]
+    ) {
+        channelConfigs.forEach(c => (this.configs[c.channelType] = c));
+    }
+
+    configs: {
+        [type: number]: IChannelConfig<Appointment, Inspector<Appointment>>;
+    } = {};
+
+    /**
+     * Checks that the object is well formed, that it meets the conditions necessary for watching and assigns it to be watched.
+     * @param obj
+     */
+    async addAppointment(obj: any) {
+        if (!obj) throw new PublicDataValidationError("No content specified.");
+
+        // look for a type argument
+        const type = obj["type"];
+        const config = this.configs[type];
+        if (!config) throw new PublicDataValidationError(`Unknown appointment type ${type}.`);
+
+        // parse the appointment
+        const appointment = config.appointment(obj);
+
+        const inspector = config.inspector(10, this.provider);
+        // inspect this appointment, an error is thrown if inspection is failed
+        await inspector.inspectAndPass(appointment);
+
+        // start watching it if it passed inspection
+        await this.watcher.addAppointment(appointment);
+
+        return appointment;
+    }
+}
