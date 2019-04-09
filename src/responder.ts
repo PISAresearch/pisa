612f00ef76b967b44500dbc92c8fde93c9010eeb
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-04-09 14:32:35 +0100

    Added logic to wait for enough confirmations

--- src/responder.ts
+++ src/responder.ts
@@ -3,6 +3,7 @@ import { ethers } from 'ethers';
 import { wait } from './utils';
 import { IAppointment, IEthereumResponse } from "./dataEntities/appointment";
 import logger from "./logger";
+import { TransactionResponse } from 'ethers/providers';
 
 /**
  * Responsible for storing the state and managing the flow of a single response.
@@ -26,9 +27,10 @@ export class ResponseFlow {
  * Represents the current state of a Response
  */
 export enum ResponseStatus {
-    Started,
-    Success,
-    Failed
+    Started,      // flow started
+    ResponseSent, // responded, but waiting for enough confirmations
+    Success,      // responded with enough confirmations
+    Failed        // response flow failed
 }
 
 /**
@@ -66,6 +68,9 @@ export abstract class Responder extends EventEmitter {
         setImmediate( () => this.emit.call(this, args) );
     }
 
+    /**
+     * This function tries to submit the transaction that resolves the dispute, protecting the channel.
+     */
     protected abstract submitStateFunction(): Promise<any>;
 
     /**
@@ -76,11 +81,24 @@ export abstract class Responder extends EventEmitter {
     protected abstract respond(responseFlow: ResponseFlow): Promise<any>;
 }
 
+/**
+ * A simple custom Error cluss to provide more details in case of a re-org.
+ */
+class ReorgError extends Error {
+    constructor(public readonly tx: TransactionResponse, ...params: any) {
+        super(...params);
+        this.name = "ReorgError";
+    }
+}
+
 export class EthereumResponder extends Responder {
+    static readonly CONFIRMATIONS_REQUIRED = 40; // number of confirmations before a transaction is considered immutable
     protected readonly contract: ethers.Contract;
+
     /**
      * @param signer The signer of the wallet associated with this responder. Each responder should have exclusive access to his wallet.
-     * @param appointment The IEthereumAppointment this object is responding to.
+     * @param appointmentId The id of the Appointment this object is responding to.
+     * @param ethereumResponse The IEthereumResponse containing what needs to be submitted.
      * @param maxAttempts The maximum number of retries before the Responder will give up.
      * TODO: docs
      */
@@ -94,7 +112,7 @@ export class EthereumResponder extends Responder {
         super(maxAttempts);
     }
 
-    protected async submitStateFunction() {
+    protected submitStateFunction(): Promise<TransactionResponse> {
         // form the interface so that we can serialise the args and the function name
         const abiInterface = new ethers.utils.Interface(this.ethereumResponse.contractAbi);
         const data = abiInterface.functions[this.ethereumResponse.functionName].encode(this.ethereumResponse.functionArgs);
@@ -108,35 +126,59 @@ export class EthereumResponder extends Responder {
         };
 
         // execute the transaction
-        return await this.signer.sendTransaction(transactionRequest);
+        return this.signer.sendTransaction(transactionRequest);
     }
 
     protected async respond(responseFlow: ResponseFlow) {
         while (responseFlow.attempts < this.maxAttempts) {
             responseFlow.attempts++;
             try {
-                await this.submitStateFunction();
+                const tx = await this.submitStateFunction();
+
+                responseFlow.status = ResponseStatus.ResponseSent;
+                this.asyncEmit("responseSent", responseFlow);
+
+                // Wait for enough confirmations before declaring success
+                await new Promise((resolve, reject) => {
+                    const provider = this.signer.provider;
+                    const newBlockHandler = async () => {
+                        const receipt = await provider.getTransactionReceipt(tx.hash);
+                        if (receipt == null) {
+                            // There was a re-org, consider this attempt failed and attempt the transaction again
+                            provider.removeListener("block", newBlockHandler);
+                            reject(new ReorgError(tx, "There could have been a re-org, the transaction was sent but was later not found."));
+                        } else if (receipt.confirmations >= EthereumResponder.CONFIRMATIONS_REQUIRED) {
+                            provider.removeListener("block", newBlockHandler);
+                            resolve();
+                        }
+                    };
+                    provider.on("block", newBlockHandler);
+                });
 
                 responseFlow.status = ResponseStatus.Success;
+                console.log("responseConfirmed");
+                this.asyncEmit("responseConfirmed", responseFlow);
 
-                this.emit("responseSuccessful", responseFlow);
                 return;
             } catch (doh) {
 
-                this.emit("attemptFailed", responseFlow, doh);
+                console.log("attemptFailed");
+                this.asyncEmit("attemptFailed", responseFlow, doh);
 
                 //TODO: implement a proper strategy
                 await wait(1000);
             }
         }
-        this.emit("responseFailed", responseFlow);
+
+        console.log("responseFailed");
+        this.asyncEmit("responseFailed", responseFlow);
     }
 }
 
 /**
  * Responsible for handling the business logic of the Responders.
  */
-// TODO: only handling Kitsune appointments for now, and only one active response.
+// TODO: only correctly handling one active response.
 //       Should add a pool of wallets to allow concurrent responses.
 
 export class EthereumResponderManager {
@@ -158,9 +200,18 @@ export class EthereumResponderManager {
         this.responders.add(responder);
 
         responder
-            .on("responseSuccessful", (responseFlow: ResponseFlow) => {
+            .on("responseSent", (responseFlow: ResponseFlow) => {
                 logger.info(
-                    `Successfully responded to appointment ${"TODO: appointment ID"} after ${response.attempts} attempt${response.attempts > 1 ? "s" : ""}.`
+                    `Successfully responded to appointment ${"TODO: appointment ID"} after ${responseFlow.attempts} attempt${responseFlow.attempts > 1 ? "s" : ""}.
+                     Waiting for enough confirmations (${EthereumResponder.CONFIRMATIONS_REQUIRED}).`
+                );
+
+                // TODO: Should we store information about past responders anywhere?
+                this.responders.delete(responder);
+            })
+            .on("responseConfirmed", (responseFlow: ResponseFlow) => {
+                logger.info(
+                    `Successfully responded to appointment ${"TODO: appointment ID"} after ${responseFlow.attempts} attempt${responseFlow.attempts > 1 ? "s" : ""}.`
                 );
 
                 // Should we keep inactive responders anywhere?
@@ -168,18 +219,18 @@ export class EthereumResponderManager {
             })
             .on("attemptFailed", (responseFlow: ResponseFlow, doh) => {
                 logger.error(
-                    `Failed to respond to appointment ${"TODO: appointment ID"}; ${response.attempts} attempt${response.attempts > 1 ? "s" : ""}.`
+                    `Failed to respond to appointment ${"TODO: appointment ID"}; ${responseFlow.attempts} attempt${responseFlow.attempts > 1 ? "s" : ""}.`
                 );
                 logger.error(doh);
             })
             .on("responseFailed", (responseFlow: ResponseFlow) => {
                 logger.error(
-                    `Failed to respond to ${"TODO: appointment ID"}, after ${response.attempts} attempt${response.attempts > 1 ? "s" : ""}. Giving up.`
+                    `Failed to respond to ${"TODO: appointment ID"}, after ${responseFlow.attempts} attempt${responseFlow.attempts > 1 ? "s" : ""}. Giving up.`
                 );
 
                 //TODO: this is serious and should be escalated.
             });
 
-        const response = responder.startResponse();
+        responder.startResponse();
     }
 }
