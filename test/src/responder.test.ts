965e8e1977a0b4d309e01f7a76b0f129c095f503
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-04-12 11:35:28 +0100

    Make a snapshot of the blockchain and restore it after each test

--- test/src/responder.test.ts
+++ test/src/responder.test.ts
@@ -19,6 +19,27 @@ describe("DedicatedEthereumResponder", () => {
     let account0: string, account1: string, channelContract: ethers.Contract, hashState: string, disputePeriod: number;
     let responderAccount: string;
 
+    let initialSnapshotId: string;
+
+    // Save a snapshot of the state of the blockchain in ganache; resolves to the id of the snapshot
+    async function takeGanacheSnapshot(): Promise<string> {
+        return await new Promise(async (resolve, reject) => {
+            ganache.sendAsync({"id": 1, "jsonrpc":"2.0", "method":"evm_snapshot", "params": []}, (err, res: any) => {
+                if (err) reject(err); else resolve(res.result);
+            });
+        });
+    }
+
+    // Restores a previously saved snapshot given the id. Note: the id _cannot_ be reused
+    async function restoreGanacheSnapshot(id: string) {
+        await new Promise(async (resolve, reject) => {
+            ganache.sendAsync({"id": 1, "jsonrpc":"2.0", "method":"evm_revert", "params": [id]}, (err, _) => {
+                if (err) reject(err); else resolve();
+            });
+        });
+    }
+
+
     before(async () => {
         // Set up the accounts
         const accounts = await provider.listAccounts();
@@ -44,7 +65,18 @@ describe("DedicatedEthereumResponder", () => {
         const account0Contract = channelContract.connect(provider.getSigner(account0));
         const tx = await account0Contract.triggerDispute();
         await tx.wait();
+
+        // Make a snapshot of the blockchain for re-using it in many tests
+        initialSnapshotId = await takeGanacheSnapshot();
     });
+
+    // Restore the initial snapshot for the next test
+    afterEach(async () => {
+        await restoreGanacheSnapshot(initialSnapshotId);
+        initialSnapshotId = await takeGanacheSnapshot();
+    });
+
+
 
     it("correctly submits an appointment to the blockchain", async () => {
         const signer = provider.getSigner(responderAccount);
