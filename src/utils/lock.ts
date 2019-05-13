3860d5afca6c2a0921a9466a7524626447f0a308
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-05-13 09:31:10 +0100

    Added documentation to Lock and related classes

--- src/utils/lock.ts
+++ src/utils/lock.ts
@@ -1,5 +1,10 @@
 import { ApplicationError } from "../dataEntities";
 
+/**
+ * Simple lock class.
+ *
+ * Use in a try/finally block, calling `release()` in the `finally` block to guarantee the lock is released in any circumstance.
+ */
 export class Lock {
     private waiters: Array<() => void> = [];
     private mLocked = false;
@@ -8,6 +13,9 @@ export class Lock {
         return this.mLocked;
     }
 
+    /**
+     * Acquires the lock; returns a promise that resolves when the lock is acquired.
+     */
     public async acquire(): Promise<void> {
         if (!this.mLocked) {
             this.mLocked = true;
@@ -19,6 +27,11 @@ export class Lock {
         }
     }
 
+    /**
+     * Released the lock.
+     *
+     * @throws ApplicationError if the called when the lock was
+     */
     public release() {
         if (!this.mLocked) throw new ApplicationError("Tried to release a Lock that was not locked.");
 
@@ -32,11 +45,21 @@ export class Lock {
     }
 }
 
+/**
+ * Represents a number of locks indexed by a string `key`.
+ * Locks are deleted once they are released, thus the memory occupation is always proportional to the
+ * number of currently locked locks.
+ */
 export class LockManager {
     private locks: {
         [key: string]: Lock;
     } = {};
 
+    /**
+     * Acquires the lock indexed by `key`.
+     *
+     * @param key
+     */
     public async acquire(key: string) {
         if (!this.locks[key]) {
             this.locks[key] = new Lock();
@@ -44,6 +67,12 @@ export class LockManager {
 
         await this.locks[key].acquire();
     }
+
+    /**
+     * Releases the lock indexed by `key`.
+     *
+     * @param key
+     */
     public release(key: string) {
         this.locks[key].release();
         if (!this.locks[key].locked) {
@@ -52,9 +81,20 @@ export class LockManager {
     }
 }
 
+/**
+ * Simple utility class to run code guarded by indexed locks.
+ */
 export class LockUtil {
     private manager = new LockManager();
 
+    /**
+     * Acquires the lock indexed by `key` and runs `func`.
+     * Resolves to the value returned by `func`, or rejects with the same error.
+     * Releases the lock once `func` is resolved or rejected.
+     *
+     * @param key
+     * @param func
+     */
     public async withLock<T>(key: string, func: () => T): Promise<T> {
         try {
             await this.manager.acquire(key);
