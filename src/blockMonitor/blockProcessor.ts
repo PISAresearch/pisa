7a0ae9e8b723ddddfb13dd9d80f8b2349371a3d4
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-05-20 17:09:21 +0100

    Simplified BlockProcessor (feat. Chris)

--- src/blockMonitor/blockProcessor.ts
+++ src/blockMonitor/blockProcessor.ts
@@ -1,6 +1,5 @@
 import { ethers } from "ethers";
 import { StartStopService } from "../dataEntities";
-import logger from "../logger";
 import { BlockCache } from "./blockCache";
 import { IBlockStub } from "./blockStub";
 
@@ -11,11 +10,6 @@ import { IBlockStub } from "./blockStub";
  * the `blockCache` with the new block and its ancestors.
  */
 export class BlockProcessor extends StartStopService {
-    // Blocks that are ready to be added to the BlockCache as soon as possible
-    private blocksToAdd: ethers.providers.Block[] = [];
-    // Hashes of blocks that are already known but not downloaded yet
-    private pendingHashes: Set<string> = new Set();
-
     private lastBlockHashReceived: string;
 
     public get head(): IBlockStub | null {
@@ -33,7 +27,6 @@ export class BlockProcessor extends StartStopService {
         super("Block processor");
 
         this.handleBlockEvent = this.handleBlockEvent.bind(this);
-        this.processQueue = this.processQueue.bind(this);
     }
 
     protected startInternal(): void {
@@ -44,69 +37,32 @@ export class BlockProcessor extends StartStopService {
         this.provider.removeListener("block", this.handleBlockEvent);
     }
 
-    private async handleBlockEvent(blockNumber: number) {
-        const block = await this.provider.getBlock(blockNumber);
-        this.lastBlockHashReceived = block.hash;
-        if (this.blockCache.hasBlock(block.hash)) {
-            // Emit the new head block, now that all its info is ready
-            this.emit(BlockProcessor.NEW_HEAD_EVENT, block.number, block.hash);
+    private async getParentsNotInCache(block: ethers.providers.Block): Promise<ethers.providers.Block[]> {
+        if (this.blockCache.canAddBlock(block)) {
+            // this parent is in the cache - do nothing
+            return [];
         } else {
-            this.enqueueBlock(block);
+            // this parent is not in the cache, find further parents also in this situation
+            const parentBlock = await this.provider.getBlock(block.parentHash);
+            return (await this.getParentsNotInCache(parentBlock)).concat(parentBlock);
         }
     }
 
-    private enqueueBlock(block: ethers.providers.Block) {
-        // If we do not already know this block
-        if (block.hash in this.blocksToAdd.map(b => b.hash)) return; // block already known
-        if (this.blockCache.hasBlock(block.hash)) return;
+    private async handleBlockEvent(blockNumber: number) {
+        const observedBlock = await this.provider.getBlock(blockNumber);
+        this.lastBlockHashReceived = observedBlock.hash;
 
-        this.blocksToAdd.push(block);
-        this.processQueue();
-    }
+        // populate block and parents in cache
+        if (!this.blockCache.hasBlock(observedBlock.hash)) {
+            (await this.getParentsNotInCache(observedBlock))
+                .concat(observedBlock)
+                .filter(b => !this.blockCache.hasBlock(b.hash))
+                .forEach(b => this.blockCache.addBlock(b));
+        }
 
-    // Process the queue until there are no blocks that can be added.
-    // Potentially O(n^2) for a queue of n in reverse order of addition.
-    private processQueue() {
-        let done = false;
-        while (!done) {
-            done = true; // Unless we remove a block from the queue, no point in retrying
-
-            const nextBlocksToAdd: ethers.providers.Block[] = [];
-            for (let block of this.blocksToAdd) {
-                // It is possible to add if the parent is already added (or node is deep enough)
-                if (this.blockCache.canAddBlock(block)) {
-                    this.blockCache.addBlock(block);
-                    if (block.hash === this.lastBlockHashReceived) {
-                        // Emit the new head block, now that all its info is ready
-                        this.emit(BlockProcessor.NEW_HEAD_EVENT, block.number, block.hash);
-                    }
-                    done = false; // as the queue changed, we repeat the processing once more
-                } else {
-                    // Put block in queue for the next iteration...
-                    nextBlocksToAdd.push(block);
-                    // ...as we need to add the parent block first (unless already pending)
-                    if (!this.pendingHashes.has(block.parentHash)) {
-                        this.pendingHashes.add(block.parentHash);
-
-                        this.provider
-                            .getBlock(block.parentHash)
-                            .then(parentBlock => {
-                                this.pendingHashes.delete(block.parentHash);
-                                this.enqueueBlock(parentBlock);
-                            })
-                            .catch(doh => {
-                                this.pendingHashes.delete(block.parentHash);
-                                // Failing to fetch a parent block is not a disaster (will retry), but log it anyway, for now
-                                logger.error(
-                                    `${this.name}: Error while fetching block ${block.parentHash}. Will try again.`
-                                );
-                            });
-                    }
-                }
-            }
-
-            // Put back in queue all blocks that were not removed yet
-            this.blocksToAdd = nextBlocksToAdd;
+        // is the observed block still the last block received?
+        if (this.lastBlockHashReceived === observedBlock.hash) {
+            this.emit(BlockProcessor.NEW_HEAD_EVENT, observedBlock.number, observedBlock.hash);
         }
     }
 }
