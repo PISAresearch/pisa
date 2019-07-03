bb6b8388bcacc39eacdeb602e7239c15efc687c3
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-07-03 11:16:03 +0100

    Naming convention for type parameters

--- src/blockMonitor/component.ts
+++ src/blockMonitor/component.ts
@@ -1,6 +1,6 @@
 import { IBlockStub } from "../dataEntities";
 
-export abstract class Component<TState, Block extends IBlockStub> {
-    public abstract reduce(prevState: TState, block: Block): TState;
-    public abstract handleNewStateEvent(prevHead: Block, prevState: TState, head: Block, state: TState): void;
+export abstract class Component<TState, TBlock extends IBlockStub> {
+    public abstract reduce(prevState: TState, block: TBlock): TState;
+    public abstract handleNewStateEvent(prevHead: TBlock, prevState: TState, head: TBlock, state: TState): void;
 }
