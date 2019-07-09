aad960af60a8f55298c0268bebf17d839b5ba0d2
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-07-09 12:03:09 +0100

    Added some documentation to component.ts

--- src/blockMonitor/component.ts
+++ src/blockMonitor/component.ts
@@ -4,30 +4,42 @@ import { IBlockStub } from "../dataEntities";
  * A base for object that define the initial anchor state and the changes in state when a new block is processed.
  */
 export interface StateReducer<TState extends object, TBlock extends IBlockStub> {
+    /**
+     *
+     * @param block
+     */
     getInitialState(block: TBlock): TState;
     reduce(prevState: TState, block: TBlock): TState;
 }
 
+/**
+ * Convenience type for a state derived from mapping strings (typically, an id) to a per-item state.
+ */
 export type MappedState<TState extends object> = Map<string, TState>;
 
 /**
  * A utility class to apply a reducer to each object of a set of objects that contains a string `id` field.
- * Each object can be used to generate an individual reducer, and this class combines them to obtain a bigger
- * anchor state as a map indexed by the same `id`.
+ * Each object is used to generate an individual reducer (here referred as "base reducer"), and this class combines them
+ * to obtain a bigger anchor state as a map indexed by the same `id`.
  */
 export class MappedStateReducer<TState extends object, TBlock extends IBlockStub, TMappedType extends { id: string }>
     implements StateReducer<MappedState<TState>, TBlock> {
     /**
      * Creates a new reducer for the given collection of objects.
      * @param getItems a function returning the current state of the collection; it is expected that
-     *                 the collection changes over time, but each item of the collection should be immutable.
-     * @param getBaseReducer a function that returns a reducer for
+     *     the collection changes over time, but each item of the collection should be immutable.
+     * @param getBaseReducer a function that returns a reducer for a given object. The function should always
+     *     return the same value when called on the same object.
      */
     constructor(
-        public getItems: () => TMappedType[],
-        public getBaseReducer: (obj: TMappedType) => StateReducer<TState, TBlock>
+        public readonly getItems: () => TMappedType[],
+        public readonly getBaseReducer: (obj: TMappedType) => StateReducer<TState, TBlock>
     ) {}
 
+    /**
+     * Computes the initial state, by using the `getInitialState` function on each base reducer.
+     * @param block
+     */
     public getInitialState(block: TBlock): MappedState<TState> {
         const result: MappedState<TState> = new Map();
         for (const obj of this.getItems()) {
@@ -36,6 +48,13 @@ export class MappedStateReducer<TState extends object, TBlock extends IBlockStub
         }
         return result;
     }
+
+    /**
+     * Computes the new state; for an object that had a previous state, it uses the `reduce` function on the base reducer.
+     * Otherwise, it computes an initial state with `getInitialState` on the base reducer.
+     * @param prevState
+     * @param block
+     */
     public reduce(prevState: MappedState<TState>, block: TBlock): MappedState<TState> {
         const result: MappedState<TState> = new Map();
         for (const obj of this.getItems()) {
@@ -52,6 +71,9 @@ export class MappedStateReducer<TState extends object, TBlock extends IBlockStub
     }
 }
 
+/**
+ * A `Component` contains a state reducer and receives and processes the state changes after being added to a `BlockchainMachine`.
+ */
 export abstract class Component<TState extends object, TBlock extends IBlockStub> {
     public constructor(public readonly reducer: StateReducer<TState, TBlock>) {}
     abstract handleNewStateEvent(prevHead: TBlock, prevState: TState, head: TBlock, state: TState): void;
