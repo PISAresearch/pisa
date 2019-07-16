c6dd1693dd5fbc8f43a45ba23124d9821fe24395
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-07-16 18:23:24 +0100

    Added initial tests for BlockchainMachine

--- src/blockMonitor/index.ts
+++ src/blockMonitor/index.ts
@@ -1,2 +1,3 @@
 export { ReadOnlyBlockCache, BlockCache, getConfirmations } from "./blockCache";
+export { BlockchainMachine } from "./blockchainMachine";
 export { BlockProcessor, blockStubAndTxHashFactory as blockStubAndTxFactory, blockFactory } from "./blockProcessor";
---
+++ test/src/blockMonitor/blockchainMachine.test.ts
@@ -0,0 +1,169 @@
+import "mocha";
+import { expect } from "chai";
+import { spy, verify, anything, capture, resetCalls } from "ts-mockito";
+import { BlockProcessor, BlockCache, BlockchainMachine } from "../../../src/blockMonitor";
+import { Component } from "../../../src/blockMonitor/component";
+import { IBlockStub, ApplicationError } from "../../../src/dataEntities";
+import { StateReducer } from "../../../src/blockMonitor/component";
+import { EventEmitter } from "events";
+
+const blocks: IBlockStub[] = [
+    {
+        hash: "hash0",
+        number: 0,
+        parentHash: "hash"
+    },
+    {
+        hash: "hash1",
+        number: 1,
+        parentHash: "hash0"
+    },
+    {
+        hash: "hash2",
+        number: 2,
+        parentHash: "hash1"
+    }
+];
+
+interface ExampleState {
+    someNumber: number;
+}
+
+const initialState = {
+    someNumber: 42
+};
+
+class ExampleReducer implements StateReducer<ExampleState, IBlockStub> {
+    getInitialState(block: IBlockStub) {
+        return initialState;
+    }
+    reduce(prevState: ExampleState, block: IBlockStub) {
+        return {
+            someNumber: prevState.someNumber + block.number
+        };
+    }
+}
+
+class ExampleComponent extends Component<ExampleState, IBlockStub> {
+    public handleChanges(prevState: ExampleState, state: ExampleState): void {}
+}
+
+describe("BlockchainMachine", () => {
+    let reducer: ExampleReducer;
+    let spiedReducer: ExampleReducer;
+    let blockProcessor: BlockProcessor<IBlockStub>;
+    let blockCache: BlockCache<IBlockStub>;
+
+    beforeEach(() => {
+        reducer = new ExampleReducer();
+        spiedReducer = spy(reducer);
+
+        blockCache = new BlockCache<IBlockStub>(100);
+        blocks.forEach(b => blockCache.addBlock(b));
+
+        // Since we only need to process events, we mock the BlockProcessor with an EventEmitter
+        const bp: any = new EventEmitter();
+        bp.blockCache = blockCache;
+        blockProcessor = bp as BlockProcessor<IBlockStub>;
+    });
+
+    it("addComponent throws ApplicationError if already started", async () => {
+        const bm = new BlockchainMachine(blockProcessor);
+        await bm.start();
+
+        expect(() => bm.addComponent(new ExampleComponent(reducer))).to.throw(ApplicationError);
+
+        await bm.stop();
+    });
+
+    it("processNewBlock computes the initial state if the parent is not in cache", async () => {
+        const bm = new BlockchainMachine(blockProcessor);
+        bm.addComponent(new ExampleComponent(reducer));
+        await bm.start();
+
+        blockProcessor.emit(BlockProcessor.NEW_BLOCK_EVENT, blocks[0]);
+
+        verify(spiedReducer.getInitialState(anything())).once();
+        verify(spiedReducer.reduce(anything(), anything())).never();
+
+        await bm.stop();
+    });
+
+    it("processNewBlock computes state with reducer and its parent's initial state if the parent's state is not known", async () => {
+        const bm = new BlockchainMachine(blockProcessor);
+        bm.addComponent(new ExampleComponent(reducer));
+        await bm.start();
+
+        blockProcessor.emit(BlockProcessor.NEW_BLOCK_EVENT, blocks[1]);
+
+        // initializer and reducer should both be called once
+        verify(spiedReducer.getInitialState(anything())).once();
+        verify(spiedReducer.reduce(anything(), anything())).once();
+
+        // Check that the reducer was called on the right data
+        const [state, block] = capture(spiedReducer.reduce).last();
+        expect(state).to.deep.equal(initialState);
+        expect(block).to.deep.equal(blocks[1]);
+
+        await bm.stop();
+    });
+
+    it("processNewBlock computes the state with the reducer if the parent's state is known", async () => {
+        const bm = new BlockchainMachine(blockProcessor);
+        bm.addComponent(new ExampleComponent(reducer));
+        await bm.start();
+
+        blockProcessor.emit(BlockProcessor.NEW_BLOCK_EVENT, blocks[0]);
+        blockProcessor.emit(BlockProcessor.NEW_BLOCK_EVENT, blocks[1]);
+
+        resetCalls(spiedReducer);
+
+        // State of the parent is { someNumber: 42 + blocks[1].number }
+
+        blockProcessor.emit(BlockProcessor.NEW_BLOCK_EVENT, blocks[2]);
+
+        verify(spiedReducer.getInitialState(anything())).never();
+        verify(spiedReducer.reduce(anything(), anything())).once();
+
+        // Check that the reducer was called on the right data
+        const [state, block] = capture(spiedReducer.reduce).last();
+        expect(state).to.deep.equal({
+            someNumber: 42 + blocks[1].number
+        });
+        expect(block).to.deep.equal(blocks[2]);
+
+        await bm.stop();
+    });
+
+    it("processNewHead correctly calls handleChanges on the component", async () => {
+        const bm = new BlockchainMachine(blockProcessor);
+        const component = new ExampleComponent(reducer);
+        const spiedComponent = spy(component);
+
+        bm.addComponent(component);
+        await bm.start();
+
+        blockProcessor.emit(BlockProcessor.NEW_BLOCK_EVENT, blocks[0]);
+        blockProcessor.emit(BlockProcessor.NEW_HEAD_EVENT, blocks[0], null);
+
+        blockProcessor.emit(BlockProcessor.NEW_BLOCK_EVENT, blocks[1]);
+
+        // Up to here, handleChanges should not have been called on the component
+        verify(spiedComponent.handleChanges(anything(), anything())).never();
+
+        blockProcessor.emit(BlockProcessor.NEW_BLOCK_EVENT, blocks[2]);
+        blockProcessor.emit(BlockProcessor.NEW_HEAD_EVENT, blocks[2], blocks[0]);
+
+        verify(spiedComponent.handleChanges(anything(), anything())).once();
+
+        // Check that handleChanges was called on the right data
+        const [prevState, newState] = capture(spiedComponent.handleChanges).last();
+
+        expect(prevState).to.deep.equal(initialState);
+        expect(newState).to.deep.equal({
+            someNumber: initialState.someNumber + blocks[1].number + blocks[2].number
+        });
+
+        await bm.stop();
+    });
+});
