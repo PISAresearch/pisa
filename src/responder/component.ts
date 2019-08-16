96275bbb74b6a230a9dd6af09d290cca11c844c7
Chris Buckland <cpbuckland88@gmail.com>
2019-08-16 17:57:56 +0200

    Added more logging for responder matches

--- src/responder/component.ts
+++ src/responder/component.ts
@@ -58,6 +58,11 @@ export class ResponderAppointmentReducer implements StateReducer<ResponderAppoin
             // therefore we fallback to network id when chain id is not present
             const txChainId = tx.chainId || (tx as any).networkId;
             const txIdentifier = new PisaTransactionIdentifier(txChainId, tx.data, tx.to, tx.value, tx.gasLimit);
+            if(tx.from.toLowerCase() === this.address.toLowerCase()) {
+                logger.info(tx, "Potential transaction match.")
+                logger.info(txIdentifier, "Formed tx identfier.")
+                logger.info(identifier, "State identifier.")
+            }
             if (txIdentifier.equals(identifier) && tx.from.toLowerCase() === this.address.toLowerCase()) {
                 return {
                     blockNumber: tx.blockNumber!,
