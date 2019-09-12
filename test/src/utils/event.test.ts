9fb31f8dce40eef098db5505bf6f50e2bd80cd49
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-09-12 12:48:02 +0700

    Added tests for new Event class

---
+++ test/src/utils/event.test.ts
@@ -0,0 +1,66 @@
+import "mocha";
+import { expect } from "chai";
+import fnIt from "../../utils/fnIt";
+import { Event, BlockEvent } from "../../../src/utils/event";
+import { IBlockStub, ApplicationError } from "../../../src/dataEntities";
+
+describe("Event", async () => {
+    const block: IBlockStub = {
+        number: 42,
+        hash: "0x4242",
+        parentHash: "0x1111"
+    };
+
+    fnIt<Event<any>>(e => e.addListener, "adds a listener and receives the event with the correct parameter", async () => {
+        const e = new BlockEvent<IBlockStub>();
+        let calledWith: any = null;
+        e.addListener(async (b: IBlockStub) => {
+            calledWith = b;
+        });
+
+        await e.emit(block);
+        expect(calledWith).to.deep.equal(block);
+    });
+
+    it("can add and remove a listener", async () => {
+        const e = new BlockEvent<IBlockStub>();
+        let calledWith: any = null;
+        const listener = async (b: IBlockStub) => {
+            calledWith = b;
+        };
+
+        e.addListener(listener);
+        e.removeListener(listener);
+
+        await e.emit(block);
+        expect(calledWith).to.be.null;
+    });
+
+    it("can add multiple listeners", async () => {
+        const e = new BlockEvent<IBlockStub>();
+        let calledWith1: any = null;
+        const listener1 = async (b: IBlockStub) => {
+            calledWith1 = b;
+        };
+        let calledWith2: any = null;
+        const listener2 = async (b: IBlockStub) => {
+            calledWith2 = b;
+        };
+
+        e.addListener(listener1);
+        e.addListener(listener2);
+
+        await e.emit(block);
+        expect(calledWith1).to.deep.equal(block);
+        expect(calledWith2).to.deep.equal(block);
+    });
+
+    fnIt<Event<any>>(e => e.removeListener, "throws ApplicationError if the listener does not exist", async () => {
+        const e = new BlockEvent<IBlockStub>();
+        const listener = async (b: IBlockStub) => {};
+        const otherListener = async (b: IBlockStub) => {};
+
+        e.addListener(listener);
+        expect(() => e.removeListener(otherListener)).to.throw(ApplicationError);
+    });
+});
