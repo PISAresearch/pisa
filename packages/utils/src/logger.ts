d49dd2f571f41f2d05e672b2e4dd305cf167f425
yahgwai <cpbuckland88@gmail.com>
2019-11-02 21:47:16 +0100

    Renamed logger to bunyan

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -2,8 +2,8 @@ import { createLogger, Stream, stdSerializers } from "bunyan";
 import fs from "fs";
 import path from "path";
 import { ArgumentError } from "@pisa-research/errors";
-import Logger from "bunyan";
-export { Logger };
+import Bunyan from "bunyan";
+export { Bunyan as Logger };
 
 const logDir = "logs";
 
@@ -60,7 +60,7 @@ let initialisedLogLevelInfo = LogLevelInfo.Info;
 let initialisedInstanceName = "not-set";
 
 // Default logger
-export const logger: Logger = createNamedLogger("main", initialisedLogLevelInfo);
+export const logger: Bunyan = createNamedLogger("main", initialisedLogLevelInfo);
 
 // Default to log level "info", unless we are running tests, then "debug"
 
