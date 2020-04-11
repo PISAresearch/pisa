517cf8720923aac81bb5fda415bd346d96f57381
yahgwai <cpbuckland88@gmail.com>
2020-04-11 18:08:37 +0200

    Version bump v0.1.34

--- packages/block/src/blockProcessor.ts
+++ packages/block/src/blockProcessor.ts
@@ -188,25 +188,26 @@ export class BlockProcessor<TBlock extends IBlockStub> extends StartStopService 
     protected async startInternal(): Promise<void> {
         // Make sure the current head block is processed
         const storeHead = await this.store.getLatestHeadNumber();
-        if (storeHead !== undefined) {
+        if (storeHead === undefined) {
             // if we're starting for the first time we wont have a store head, subscribers may want to process this
             // opening block
+            const currentHead = await this.provider.getBlockNumber();
             this.mBlockCache.newBlock.addListener(this.processNewBlock);
-            await this.processBlockNumber(storeHead);
+            await this.processBlockNumber(currentHead);
 
             this.provider.on("block", this.processBlockNumber);
 
-            this.logger.info({ currentHeadNumber: storeHead }, "Blockprocessor started.");
+            this.logger.info({ currentHeadNumber: currentHead }, "Blockprocessor started.");
         } else {
             // if this isnt the first time processing then we process this block number just to set the head
-            const currentHead = await this.provider.getBlockNumber();
-            await this.processBlockNumber(currentHead);
+
+            await this.processBlockNumber(storeHead);
             this.provider.on("block", this.processBlockNumber);
 
             // After startup, `newBlock` events of the BlockCache are proxied
             this.mBlockCache.newBlock.addListener(this.processNewBlock);
 
-            this.logger.info({ currentHeadNumber: currentHead }, "Blockprocessor started.");
+            this.logger.info({ currentHeadNumber: storeHead }, "Blockprocessor started.");
         }
     }
 
